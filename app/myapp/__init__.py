afa92c0dfeb9445d31c238f483cd6ff368ebbf3d
josuergmx <josuergmx@gmail.com>
2024-10-16 15:26:31 -0600

    Templates con Jinja

--- app/myapp/__init__.py
+++ app/myapp/__init__.py
@@ -3,12 +3,12 @@
     tales como la importacion de la app, las rutas generales del proyecto, definir si el
     servidor corre en modo debug, Registro de la BD, etc.'''
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 from myapp.config import DevConfig
 from myapp.tasks.controllers import taskRoute
 
-app = Flask(__name__)
+app = Flask(__name__) #template_folder="/pages"
 app.register_blueprint(taskRoute)
 
 #app.debug = True
@@ -17,4 +17,6 @@ app.config.from_object(DevConfig)
 
 @app.route('/')
 def hello_world() -> str:
-    return ' Hello world'
+    name = request.args.get('name', 'Valor por defecto')
+    return render_template('index.html', task="Josue", name=name)
+    #return ' Hello world'
--- app/myapp/tasks/controllers.py
+++ app/myapp/tasks/controllers.py
@@ -1,11 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
 
 taskRoute = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+task_list = ['tasks 1', 'tasks 2', 'tasks 3']
 
 #Creacion de rutas en el modulo rutas
 @taskRoute.route('/')
 def index():
-    return 'Index'
+    return render_template('dashboard/task/index.html', tasks=task_list)
 
 @taskRoute.route('/<int:id>')
 def show(id:int):
@@ -13,12 +15,24 @@ def show(id:int):
 
 @taskRoute.route('/delete/<int:id>')
 def delete(id:int):
-    return 'delete ' +str(id)
+   
+    if id is not None and id is not "":
+        del task_list[id]
+        return redirect(url_for('tasks.index'))
 
 @taskRoute.route('/create', methods=('GET','POST'))
 def create():
-    return 'Create '
+    task = request.form.get('task')
+    if task is not None and task is not "":
+        task_list.append(task)
+        return redirect(url_for('tasks.index'))
+    return render_template('dashboard/task/create.html')
 
 @taskRoute.route('/update/<int:id>', methods=['GET','POST'])
 def update(id:int):
-    return 'update '+str(id)
+    task = request.form.get('task')
+    if task is not None and task is not "":
+        task_list[id] = task
+        return redirect(url_for('tasks.index'))
+        
+    return render_template('dashboard/task/update.html')
